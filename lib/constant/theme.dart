1a40ae5d56cea3b04431b8aacbb1a7255c6068bb
xun082 <73689580+xun082@users.noreply.github.com>
2025-09-02 18:26:08 +0800

    优化代码

--- lib/constant/theme.dart
+++ lib/constant/theme.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.black,
-    brightness: Brightness.dark,
-  ),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.black, brightness: Brightness.dark),
   useMaterial3: true,
   scaffoldBackgroundColor: Colors.black,
   appBarTheme: const AppBarTheme(
@@ -13,28 +10,12 @@ ThemeData darkTheme = ThemeData(
     surfaceTintColor: Colors.transparent,
     elevation: 0,
   ),
-  bottomAppBarTheme: const BottomAppBarTheme(
-    color: Colors.black,
-    elevation: 0,
-    surfaceTintColor: Colors.transparent,
-  ),
+  bottomAppBarTheme: const BottomAppBarThemeData(color: Colors.black, elevation: 0, surfaceTintColor: Colors.transparent),
   splashFactory: NoSplash.splashFactory,
   highlightColor: Colors.transparent,
   splashColor: Colors.transparent,
   hoverColor: Colors.transparent,
-  textButtonTheme: const TextButtonThemeData(
-    style: ButtonStyle(
-      overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent),
-    ),
-  ),
-  elevatedButtonTheme: const ElevatedButtonThemeData(
-    style: ButtonStyle(
-      overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent),
-    ),
-  ),
-  outlinedButtonTheme: const OutlinedButtonThemeData(
-    style: ButtonStyle(
-      overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent),
-    ),
-  ),
+  textButtonTheme: const TextButtonThemeData(style: ButtonStyle(overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent))),
+  elevatedButtonTheme: const ElevatedButtonThemeData(style: ButtonStyle(overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent))),
+  outlinedButtonTheme: const OutlinedButtonThemeData(style: ButtonStyle(overlayColor: WidgetStatePropertyAll<Color?>(Colors.transparent))),
 );
